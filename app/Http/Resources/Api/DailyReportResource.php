5f769d586b9d497c8e801963651a7d8c8e64ace5
Pramtoxz <pramuditometra@gmail.com>
2026-05-01 14:28:49 +0700

    fix: detail daily report

--- app/Http/Resources/Api/DailyReportResource.php
+++ app/Http/Resources/Api/DailyReportResource.php
@@ -18,27 +18,43 @@ class DailyReportResource extends JsonResource
             'id' => $this->id,
             'siswa_id' => $this->siswa_id,
             'tanggal' => $this->tanggal?->format('Y-m-d'),
-            'jam_masuk' => $this->jam_masuk,
-            'jam_pulang' => $this->jam_pulang,
-            'suhu_tubuh' => $this->suhu_tubuh,
-            'kondisi_umum' => $this->kondisi_umum,
-            'catatan_kesehatan' => $this->catatan_kesehatan,
-            'makan_pagi' => $this->makan_pagi,
+            
+            // Mood & Activity
+            'mood' => $this->mood,
+            'activity' => $this->activity,
+            
+            // Menu Makanan dengan status
+            'sarapan_pagi' => $this->sarapan_pagi,
+            'sarapan_status' => $this->sarapan_status,
             'makan_siang' => $this->makan_siang,
-            'makan_snack' => $this->makan_snack,
-            'catatan_makan' => $this->catatan_makan,
-            'tidur_siang_mulai' => $this->tidur_siang_mulai,
-            'tidur_siang_selesai' => $this->tidur_siang_selesai,
-            'kualitas_tidur' => $this->kualitas_tidur,
-            'bab' => $this->bab,
+            'makan_siang_status' => $this->makan_siang_status,
+            'snack_sore' => $this->snack_sore,
+            'snack_status' => $this->snack_status,
+            
+            // Minum
+            'minum_air_putih' => $this->minum_air_putih,
+            'minum_susu' => $this->minum_susu,
+            
+            // Tidur & Toilet dengan frekuensi
+            'tidur_siang' => $this->tidur_siang,
+            'tidur_siang_durasi' => $this->tidur_siang_durasi,
             'bak' => $this->bak,
-            'catatan_eliminasi' => $this->catatan_eliminasi,
-            'kegiatan_pembelajaran' => $this->kegiatan_pembelajaran,
-            'perkembangan_anak' => $this->perkembangan_anak,
-            'catatan_guru' => $this->catatan_guru,
+            'bak_frekuensi' => $this->bak_frekuensi,
+            'bab' => $this->bab,
+            'bab_frekuensi' => $this->bab_frekuensi,
+            
+            // Kebutuhan & Catatan
+            'kebutuhan_besok' => $this->kebutuhan_besok,
+            'catatan_khusus' => $this->catatan_khusus,
+            'catatan_insiden' => $this->catatan_insiden,
+            
+            // Foto
             'foto_kegiatan' => $this->foto_kegiatan,
+            
             'created_at' => $this->created_at?->toISOString(),
             'updated_at' => $this->updated_at?->toISOString(),
+            
+            // Relations
             'siswa' => new SiswaResource($this->whenLoaded('siswa')),
             'emosis' => $this->whenLoaded('emosis'),
         ];
